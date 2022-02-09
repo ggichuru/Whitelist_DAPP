cdca4529f6f39cbad90043ea44333b925cd437dc
ggichuru <ggichuru.dev@gmail.com>
2022-02-09 23:24:03 +0300

    Whitelist smart contract

---
+++ contracts/Whitelist.sol
@@ -0,0 +1,37 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Whitelist {
+    // Maximum number of whitelisted addresses allowed
+    uint8 public maxWhitelistedAddresses;
+
+    // if an address is whitelisted, set true
+    mapping(address => bool) public whitelistedAddresses;
+
+    // keep track of how many addresses have been whitelisted
+    uint8 public numAddressesWhitelisted;
+
+    // User set number of whitelisted assets at deployment
+    constructor(uint8 _maxWhitelistedAddresses) {
+        maxWhitelistedAddresses = _maxWhitelistedAddresses;
+    }
+
+    /**
+        Add the address of the sender to the whitelist
+    */
+    function addAddressToWhitelist() public {
+        require(
+            !whitelistedAddresses[msg.sender],
+            "Sender has already been whitelisted"
+        );
+        require(
+            numAddressesWhitelisted < maxWhitelistedAddresses,
+            "LIMIT REACHED! (more addresses cannot be added)"
+        );
+
+        // Add user address which called the function to the whitelistedAddresses array
+        whitelistedAddresses[msg.sender] = true;
+
+        numAddressesWhitelisted += 1;
+    }
+}

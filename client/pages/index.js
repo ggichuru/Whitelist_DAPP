1edacb3249cd843a3548e6f9bbd6ae9022b4a494
ggichuru <ggichuru.dev@gmail.com>
2022-02-10 21:12:43 +0300

    UI render

--- client/pages/index.js
+++ client/pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
   const [numberOfWhitelisted, setNumberOfWhitelisted] = useState(0)
 
   // Web3 modal (for connecting metamask) reference, which persists as long as the page is open
-  const Web3ModalRef = useRef()
+  const web3ModalRef = useRef()
 
 
   /**
@@ -21,7 +21,7 @@ export default function Home() {
    */
   const getProviderOrSigner = async (needSigner = false) => {
     //connect metamask
-    const provider = await Web3ModalRef.current.connet()
+    const provider = await web3ModalRef.current.connect();
     const web3Provider = new providers.Web3Provider(provider)
 
     const { chainId } = await web3Provider.getNetwork();
@@ -51,7 +51,7 @@ export default function Home() {
       )
 
 
-      const tx = await whitelistContract.addAddressToWhitelist()
+      const tx = await whitelistContract.addAddressToWhitelist({ gasLimit: 3000000 })
 
       setLoading(true)
 
@@ -83,4 +83,101 @@ export default function Home() {
 
     }
   }
+
+
+  const checkIfAddressInWhitelist = async () => {
+    try {
+      const signer = await getProviderOrSigner(true)
+
+      const whitelistContract = new Contract(
+        WHITELIST_CONTRACT_ADDRESS,
+        abi,
+        signer
+      )
+
+      const address = await signer.getAddress()
+
+      const _joinedWhitelist = await whitelistContract.whitelistedAddresses(address)
+
+      setJoinedWhitelist(_joinedWhitelist)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const connectWallet = async () => {
+    try {
+      await getProviderOrSigner()
+      setWalletConnected(true)
+
+      checkIfAddressInWhitelist()
+      getNumberOfWhitelisted()
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const renderButton = () => {
+    if (walletConnected) {
+      if (joinedWhiteList) {
+        return (
+          <div className={styles.description}>
+            Thanks for joining the whitelist
+          </div>
+        )
+      } else if (loading) {
+        return <button className={styles.button}>loading ... </button>
+      } else {
+        return (
+          <button onClick={addAddressToWhitelist} className={styles.button}>
+            Join the whitelist
+          </button>
+        )
+      }
+    } else {
+      return (
+        <button onClick={connectWallet} className={styles.button}>
+          Connect Wallet
+        </button>
+      )
+    }
+  }
+
+  useEffect(() => {
+    if (!walletConnected) {
+      web3ModalRef.current = new Web3Modal({
+        network: "rinbey",
+        providerOptions: {},
+        disableInjectedProvider: false
+      })
+      connectWallet()
+    }
+  }, [walletConnected])
+
+  return (
+    <div>
+      <Head>
+        <title>Whitelist Dapp (GG)</title>
+        <meta name="description" content="Whitelist-Dapp" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className={styles.main}>
+        <div>
+          <h1 className={styles.title}>Welcome to Crypto Devs</h1>
+          <div className={styles.description}>
+            {numberOfWhitelisted} have already joined the whitelist
+          </div>
+          {renderButton()}
+        </div>
+        <div>
+          <img className={styles.image} src="./crypto-devs.svg" />
+        </div>
+      </div>
+
+      <footer className={styles.footer}>
+        Made with &#10084; by GG
+      </footer>
+    </div>
+  )
 }
